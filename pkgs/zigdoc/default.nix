9376c4d7077d0b3d6348fcad3d24dee17c8a5b5d
Matt Robenolt <m@robenolt.com>
2026-04-10 14:39:46 -0700

    fix zigdoc dep cache path for linux sandbox

--- pkgs/zigdoc/default.nix
+++ pkgs/zigdoc/default.nix
@@ -11,8 +11,7 @@ let
   # The hash corresponds to the package hash in build.zig.zon.
   ziglintDep = fetchzip {
     url = "https://github.com/rockorager/ziglint/archive/refs/tags/v0.5.2.tar.gz";
-    hash = "sha256-YjFhaA5bv4M9LaMOHWTy4WtHatvcfIOCI6c0/5Lokhs=";
-    stripRoot = false;
+    hash = "sha256-Q+iJ4vTqIm8FatvW8GdkmPEkIn7AEoMHHIhoWy+eYMs=";
   };
 in
 stdenvNoCC.mkDerivation {
@@ -34,9 +33,8 @@ stdenvNoCC.mkDerivation {
   '';
 
   preBuild = ''
-    export HOME=$TMPDIR
-    mkdir -p "$HOME/.cache/zig/p"
-    cp -r ${ziglintDep} "$HOME/.cache/zig/p/1220a87cf8c8d73c1ccb37e3fbcc488b4b6b29a73c2b936aae15f88a12b52c8c76e0"
+    mkdir -p "$ZIG_GLOBAL_CACHE_DIR/p"
+    cp -r ${ziglintDep} "$ZIG_GLOBAL_CACHE_DIR/p/1220a87cf8c8d73c1ccb37e3fbcc488b4b6b29a73c2b936aae15f88a12b52c8c76e0"
   '';
 
   meta = with lib; {
